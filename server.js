8d17a5dcaf3ddde95ea751c41aaffad001d278c0
Geovanna Gabrielly de Assunção <assuncao.geovanna@escola.pr.gov.br>
2026-04-06 23:27:26 +0000

    desisto

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Isso libera o acesso para o seu site não ser bloqueado pelo navegador
+app.use(cors());
+
+app.get('/mensagem', (req, res) => {
+  res.json({ texto: "Olá! Esta mensagem veio do Back-End! 🚀" });
+});
+
+app.listen(3000, () => console.log("Servidor ligado na porta 3000"));
